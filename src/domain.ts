9119131f9abc3dffb5fafd32697caf2efa7481e1
Jesse Carter <jesse.r.carter@gmail.com>
2018-04-24 18:18:57 -0400

    Entity Decorator 	- Made decorator a generic factory 	- Selector functions for picking partitioning and clustering 	keys 	- Type safety using keyof

--- src/domain.ts
+++ src/domain.ts
@@ -1,3 +1,5 @@
 export type Ctor = new (...args: any[]) => {}
 
 export type TypedCtor<T> = new (...args: any[]) => T
+
+export type KeySelectorFn<T> = () => (keyof T)[]; 
--- src/entity.decorator.ts
+++ src/entity.decorator.ts
@@ -1,51 +1,41 @@
 import 'reflect-metadata';
-import { Ctor } from './domain';
+import { Ctor, TypedCtor, KeySelectorFn } from './domain';
 
 const EntityMetaSymbol = Symbol('EntityMeta');
 
-export interface EntityMetadata {
+export interface EntityMetadata<T> {
     keyspace: string;
-    table?: string;
+    table: string;
+    partitionKeys: KeySelectorFn<T>;
+    clusteringKeys: KeySelectorFn<T>;
 }
 
-export function Entity(meta: EntityMetadata) {
-    return (ctor: Ctor) => {
-        const entityMeta = getAllEntityMeta() || new Map<Ctor, EntityMetadata>();
+export function Entity<T>(meta: EntityMetadata<T>) {
+    return (ctor: TypedCtor<T>) => {
+        const entityMeta = getAllEntityMeta<T>() || new Map<Ctor, EntityMetadata<T>>();
         entityMeta.set(ctor, meta);
 
-        Reflect.defineMetadata(EntityMetaSymbol, entityMeta, Reflect);
+        Reflect.defineMetadata(EntityMetaSymbol, entityMeta, Reflect);   
     }
 }
 
-export function getAllEntityMeta(): Map<Ctor, EntityMetadata> {
+export function getAllEntityMeta<T>(): Map<TypedCtor<T>, EntityMetadata<T>> {
     return Reflect.getMetadata(EntityMetaSymbol, Reflect)
 }
 
-export function getEntityMeta(entityCtor: Ctor): EntityMetadata | undefined {
+export function getEntityMeta<T>(entityCtor: TypedCtor<T>): EntityMetadata<T> | undefined {
     const allMeta = getAllEntityMeta();
     return allMeta.get(entityCtor);
 }
 
-@Entity({
-    keyspace: 'iot',
-    table: ''
+@Entity<Sensor>({ 
+    keyspace: 'iot', 
+    table: '',
+    partitionKeys: () => { return ['id'] },
+    clusteringKeys: () => { return ['timestamp'] }
 })
 class Sensor {
-
-}
-
-@Entity({
-    keyspace: 'iot',
-    table: ''
-})
-class Device {
-
-}
-
-@Entity({
-    keyspace: 'iot',
-    table: ''
-})
-class Asset {
-
+    public id!: string;
+    public display!: string;
+    public timestamp!: Date;   
 }
