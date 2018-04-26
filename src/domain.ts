5730a26967f53d05b5e2abb5e8dffe2f877073ee
Jesse Carter <jesse.r.carter@gmail.com>
2018-04-25 20:47:56 -0400

    get() on repository working properly. unit tests included

--- src/domain.ts
+++ src/domain.ts
@@ -3,3 +3,31 @@ export type Ctor = new (...args: any[]) => {}
 export type TypedCtor<T> = new (...args: any[]) => T
 
 export type KeySelectorFn<T> = () => (keyof T)[]; 
+
+export const FailureSymbol = Symbol('Failure');
+
+export interface IFailure {
+    error?: Symbol;
+}
+
+export type Either<T, U> = T | U;
+
+export function isFailure<T, U extends IFailure>(either: T | U): either is U {
+    const candidateFailure = either as U;
+    if (candidateFailure.error) {
+        return candidateFailure.error === FailureSymbol;
+    }
+
+    return false;
+}
+
+export function makeFailure<T extends object>(failureObj: T) : T & IFailure {
+    const failure = <T & IFailure>{};
+    failure.error = FailureSymbol;
+
+    for (let x in failureObj) {
+        failure[x] = (failureObj as any)[x];
+    }
+
+    return failure;
+}
--- src/repository.ts
+++ src/repository.ts
@@ -1,11 +1,21 @@
-import { TypedCtor } from './domain';
+import { TypedCtor, Either, makeFailure } from './domain';
 import { Entity, EntityMetadata, getEntityMeta } from './entity.decorator';
 import { Client } from 'cassandra-driver';
+
+export interface MissingPartitionKeys {
+    keys: string[];
+}
+
+export function normalizeQueryText(queryText: string): string {
+    return queryText.replace(/\s\s+/g, ' ').trim();
+}
 
 export class Repository<T> {
     readonly entityCtor: TypedCtor<T>;
     readonly metadata: EntityMetadata<T>;
     readonly client: Client;
+    readonly partitionKeys: (keyof T)[];
+    readonly clusteringKeys: (keyof T)[];
 
     constructor(client: Client, entityCtor: TypedCtor<T>) {
         this.client = client;
@@ -16,15 +26,47 @@ export class Repository<T> {
         }
 
         this.metadata = metadata;
+        this.partitionKeys = this.metadata.partitionKeys();
+        this.clusteringKeys = this.metadata.clusteringKeys();
     }
 
-    async getByKeys2(keys: Partial<T>): Promise<T[] | undefined> {
-        return undefined;
+    async get(keys: Partial<T>): Promise<T[] | MissingPartitionKeys> {
+        // Ensure that the all partition key values have been supplied
+        // We can also build up the list of query params assuming keys are
+        // valid
+
+        const pkMap = this.partitionKeys.map((pk) => {
+            return {
+                key: pk,
+                value: keys[pk] || undefined
+            }
+        });
+
+        const missing = pkMap.filter(x => x.value === undefined)
+                              .map(x => x.key);
+
+        if (missing.length) {
+            return makeFailure({
+                keys: missing
+            });
+        }
+
+        const whereClause = pkMap.map((x, i) => {
+            return i > 0 
+                ? ` AND ${x.key} = ?`
+                : `${x.key} = ?`
+        }).join(''); /* ? */
+
+        const query = normalizeQueryText(`
+            SELECT * FROM ${this.metadata.keyspace}.${this.metadata.table}
+            WHERE ${whereClause};
+        `);
+
+        const results = await this.client.execute(query.trim(), pkMap.map(x => x.value));
+        return results.rows.map(x => ({} as T));
     }
 
     async getByKeys(keys: Partial<T>): Promise<string> {
-        console.log(keys);
-        
         const partitionKeys = this.metadata.partitionKeys();
 
         if (Object.keys(keys).length !== partitionKeys.length) {
