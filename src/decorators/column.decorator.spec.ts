d760da13adfcdac4214b17f251532fe66bde7a36
Jesse Carter <jcarter@carnegietechnologies.com>
2018-04-27 11:58:06 -0400

    Travis CI integration

--- src/decorators/column.decorator.spec.ts
+++ src/decorators/column.decorator.spec.ts
@@ -1,1 +1,7 @@
 import { Column, ColumnConfig, ColumnMetadata } from './column.decorator';
+
+describe('Column', () => {
+    it('should run a test', () => {
+        expect(42).toEqual(42);
+    });
+});
---
+++ src/decorators/entity.decorator.spec.ts
@@ -0,0 +1,7 @@
+import {} from './entity.decorator';
+
+describe('Entity', () => {
+    it('should run a test', () => {
+        expect(42).toEqual(42);
+    });
+});
